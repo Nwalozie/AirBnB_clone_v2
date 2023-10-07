220c640ba44eb1037d265380ecf836f510862779
Nwalozie <iyekcnwalozie@gmail.com>
2023-10-07 02:18:17 -0700

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,12 +8,12 @@ web_static_dir="/data/web_static"
 test_release_dir="$web_static_dir/releases/test"
 current_dir="$web_static_dir/current"
 
-mkdir -p "$web_static_dir" "$test_release_dir" "$current_dir" "$web_static_dir/shared"
-echo "Test Page" > "$test_release_dir/index.html"
-ln -s "$test_release_dir" "$current_dir"
+sudo mkdir -p "$web_static_dir" "$test_release_dir" "$current_dir" "$web_static_dir/shared"
+sudo echo "Test Page" > "$test_release_dir/index.html"
+sudo ln -sf "$test_release_dir" "$current_dir"
 
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-service nginx restart
+sudo service nginx restart

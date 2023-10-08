7eeaae52d82bdf2c48dfa2ca8d3edd7d41a86e9d
Nwalozie <iyekcnwalozie@gmail.com>
2023-10-08 13:36:04 -0700

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,6 +3,7 @@
 
 apt-get update
 apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
 web_static_dir="/data/web_static"
 test_release_dir="$web_static_dir/releases/test"
@@ -12,6 +13,7 @@ sudo mkdir -p "$web_static_dir" "$test_release_dir" "$current_dir" "$web_static_
 sudo echo "Test Page" > "$test_release_dir/index.html"
 sudo ln -sf "$test_release_dir" "$current_dir"
 
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
